df9ce806643f1c6bfefa8397bd2bb78e5c9b7e03
whitlockj <96464820+whitlockj@users.noreply.github.com>
2024-05-13 07:40:52 +0000

    Add files via upload

---
+++ randomfeatures.py
@@ -0,0 +1,11 @@
+feature_concepts = [
+    "💰 Token Rewards: Earn tokens by chatting and trading stocks. Don't forget to check out Bird's Corner Store with `/store`! Use `/econhelp` to know how it all works",
+    "📈 Stock Prices: Check stock prices with `/stock_price`. Learn more with `/econhelp`. Don't forget to check out Bird's Corner Store with `/store !",
+    "📉 Recession Alert: Prevent recession with activity. Use `/econhelp` to know how it works. Use /recession to see stats. Don't forget to check out Bird's Corner Store with `/store`!",
+    "🛍️ The Store: Shop for fun items and bonuses with `/store`. Learn about it and more with `/econ_help`. So, don't forget to check out Bird's Corner Store! Use `/econhelp` to know how it all works",
+    "📊 Stock Price Graphs: View past trends with `/stock_price`. Check `/econ_help`. Don't forget to check out Bird's Corner Store with `/store`! Use `/econhelp` to know how it all works",
+    "🎉 Token Events: Win prizes with high token gains. Check `/econ_help`. Don't forget to check out Bird's Corner Store with `/store`! Use `/econhelp` to know how it all works",
+    "🔄 Swing Trading: Trade stocks for profit. Don't forget to check out Bird's Corner Store with `/store`! Find stock commands and more with `/econhelp` to know how it all works",
+    "💼 Balance Inquiry: Check your balance with `/balance`. Don't forget to check out Bird's Corner Store with `/store`! Use `/econhelp` to know how it all works",
+    "🤝 Tip System: Tip users with `/tip`. Add a nice message with the tip. Don't forget to check out Bird's Corner Store with `/store`!  Use `/econhelp` to know how it all works"
+]
